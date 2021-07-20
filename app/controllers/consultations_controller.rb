9c4f23c86aaa968b04be63767a60b7466207b267
Namkhing Lee <nonlapat.lee@gmail.com>
2021-07-20 19:40:51 +0700

    Add validation to booking and consultation model

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,2 +1,16 @@
 class ConsultationsController < ApplicationController
+  before_action :find_consultation, only: [:show]
+
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def find_consultation
+    @consultation = Consultation.find(params[:id])
+  end
+
 end
