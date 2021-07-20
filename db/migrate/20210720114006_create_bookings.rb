84f6c8a6a10e55b0e45c7daeb61c9ea687fdc1a7
Yui Kondo <yuikondobook@gmail.com>
2021-07-20 21:09:34 +0900

    model error fixed

--- db/migrate/20210720114006_create_bookings.rb
+++ db/migrate/20210720114006_create_bookings.rb
@@ -3,11 +3,11 @@ class CreateBookings < ActiveRecord::Migration[6.0]
     create_table :bookings do |t|
       t.references :user, null: false, foreign_key: true
       t.references :consultation, null: false, foreign_key: true
-      t.string :date_time
-      t.datetime :
-      t.enum :status
+      t.date :date_time
+      t.integer :status
 
       t.timestamps
     end
   end
 end
+
