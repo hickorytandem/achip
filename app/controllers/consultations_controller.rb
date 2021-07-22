8b911144b917709c617bfda58e207ccd939ae8ba
Michael Carter <hickorytandem@gmail.com>
2021-07-22 20:27:31 +0900

    finished index

--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -1,7 +1,9 @@
 class ConsultationsController < ApplicationController
   before_action :find_consultation, only: [:show]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
+    @consultations = policy_scope(Consultation.all)
   end
 
   def show
