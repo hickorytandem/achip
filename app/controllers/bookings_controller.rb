2bcfc28fc0f90dd610d9013932a9bb5b9e6de065
Yui Kondo <yuikondobook@gmail.com>
2021-07-22 16:56:27 +0900

    edited booking& consultation controller, booking#new, consultation#index, add status to user

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,36 @@
 class BookingsController < ApplicationController
+  before_action :find_consultation, only: [:new, :create]
+
+  def index
+    @bookings = Booking.all
+  end
+  
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params) 
+    @booking.consultation = @consultation
+    if @booking.save
+      redirect_to consultation_path(@consultation)
+    else
+      render :new
+    end
+  end
+  
+  private
+  
+  def booking_params
+    params.require(:booking).permit(:date_time, :status)
+  end
+  
+  def find_consultation
+    @consultation = Consultation.find(params[:consultation_id])
+  end
+
 end
--- app/controllers/consultations_controller.rb
+++ app/controllers/consultations_controller.rb
@@ -2,13 +2,31 @@ class ConsultationsController < ApplicationController
   before_action :find_consultation, only: [:show]
 
   def index
+    @consultations = Consultation.all
+  end
+
+  def new
+    @consultation = Consultation.new
   end
 
   def show
     authorize @consultation
   end
 
+  def create
+    @consultation = Consultation.new(consultation_params) 
+    if @consultation.save
+      redirect_to consultation_path(@consultation)
+    else
+      render :new
+    end
+  end
+
   private
+
+  def consultation_params
+    params.require(:consultation).permit(:title, :description, :price, :duration)
+  end
 
   def find_consultation
     @consultation = Consultation.find(params[:id])
