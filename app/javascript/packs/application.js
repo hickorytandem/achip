d0ba20883f991263427b7f5edf3c4907132e34a3
Anna Nonaka <annanonaka@gmail.com>
2021-07-23 18:21:05 +0900

    carousel done & deleted small&medium container for better responsiveness

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,9 @@ require("channels")
 
 // External imports
 import "bootstrap";
+// Owl Carousel
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -31,6 +34,23 @@ import "bootstrap";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    responsive:{
+      0:{
+        items:1
+       },
+      600:{
+       items:3
+       },
+      1000: {
+          items: 4
+      }
+     },
+     nav: true,
+     navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"]
+   })
 });
 
 import "controllers"
