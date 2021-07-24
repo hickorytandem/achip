78d1d9ff146b8a43661221a154ed9894af2f0804
Namkhing Lee <nonlapat.lee@gmail.com>
2021-07-24 13:05:00 +0700

    Fix product banner

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
     @expertises = ["IT", "Media", "Sales", "Editing", "UX Design"]
     @consultations = policy_scope(Consultation.all)
+    @trendings = @consultations.first(6)
     @top_consultations = @consultations.sample(6)
   end
 
