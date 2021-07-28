18de4d01970b82441653b7e1cf371c2209fa73a5
Namkhing Lee <nonlapat.lee@gmail.com>
2021-07-28 23:02:16 +0700

    Fix default avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-  def avatar_for(user, width = '', height = '')
+  def avatar_for(user, class_name)
     @avatar = user.avatar
     if @avatar.attached?
-      @avatar_user = cl_image_tag(@avatar.key, alt: user.name, width: width, height: height)
+      @avatar_user = cl_image_tag(@avatar.key, alt: user.name, class: class_name)
     else
-      @avatar_user = image_tag("default_avatar.png", alt: user.name, width: width, height: height)
+      @avatar_user = image_tag("default_avatar.png", alt: user.name, class: class_name)
     end
     return @avatar_user
   end
