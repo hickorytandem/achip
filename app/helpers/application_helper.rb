7fa3d721aec9adb4e2d161dc60055bee5692893e
Namkhing Lee <nonlapat.lee@gmail.com>
2021-07-28 21:51:49 +0700

    Display default avatar whenever avatar is nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def avatar_for(user, width = '', height = '')
+    @avatar = user.avatar
+    if @avatar.attached?
+      @avatar_user = cl_image_tag(@avatar.key, alt: user.name, width: width, height: height)
+    else
+      @avatar_user = image_tag("default_avatar.png", alt: user.name, width: width, height: height)
+    end
+    return @avatar_user
+  end
 end
